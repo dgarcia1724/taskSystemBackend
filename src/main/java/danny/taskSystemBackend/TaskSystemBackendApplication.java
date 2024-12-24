383b37e9664b14ae04b48839382bd4a40339aa53
Daniel Garcia <dannyg17017@gmail.com>
2024-12-24 03:03:58 -0800

    fresh start

---
+++ src/main/java/danny/taskSystemBackend/TaskSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package danny.taskSystemBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSystemBackendApplication.class, args);
+	}
+
+}
